7530e7776b06bc540f509be03567d949ae643b44
TSyntax <105598618+TSyntax@users.noreply.github.com>
2022-08-11 10:29:16 +0100

    Merchant Update Fix

---
+++ src/main/java/com/example/agentservice/dto/MerchantUpdateDto.java
@@ -0,0 +1,15 @@
+package com.example.agentservice.dto;
+
+import lombok.Data;
+
+@Data
+public class MerchantUpdateDto {
+
+    private String city;
+    private String dateOfBirth;
+    private String firstName;
+    private String gender;
+    private String phoneNumber;
+    private String surname;
+    private String officeAddress;
+}
